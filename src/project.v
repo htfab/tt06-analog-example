81dcdd237cc122828c86b048d46b943075c382b7
htfab <mpw@htamas.net>
2024-05-05 04:04:19 +0200

    add project files

--- src/project.v
+++ src/project.v
@@ -1,8 +1,11 @@
+/*
+ * Copyright (c) 2024 Your Name
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 `default_nettype none
 
-// just a stub to keep the Tiny Tapeout tools happy
-
-module tt_um_example (
+module tt_um_analog_example (
     input  wire       VGND,
     input  wire       VPWR,
     input  wire [7:0] ui_in,    // Dedicated inputs
@@ -10,8 +13,8 @@ module tt_um_example (
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    inout  wire [7:0] ua, // analog pins
-    input  wire       ena,      // will go high when the design is enabled
+    inout  wire [7:0] ua,       // Analog pins, only ua[5:0] can be used
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
